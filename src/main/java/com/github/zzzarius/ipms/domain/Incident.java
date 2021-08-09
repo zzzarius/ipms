809453675b4144e64eb9be18e7a2d311830b3011
Darius Zivertas <darius@adaptagency.com>
2021-08-09 17:48:50 +0300

    Organize imports for Incident.java

--- src/main/java/com/github/zzzarius/ipms/domain/Incident.java
+++ src/main/java/com/github/zzzarius/ipms/domain/Incident.java
@@ -5,8 +5,15 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
